42116dbbd98f007c399c20a8c4da6c5f8d309659
Mickaël Gremy <gremy164@gmail.com>
2020-10-12 14:46:58 +0200

    Q4

--- js/script.js
+++ js/script.js
@@ -1,36 +1,63 @@
-// Fonction appelée lors du click du bouton
 function start() {
-          
-    // Création de l'objet apiWeather
-    const apiWeather = new API_WEATHER(document.getElementById("city-input").value);
-    
-    // Appel de la fonction fetchTodayForecast
-    apiWeather
-    
-        .fetchTodayForecast()
-        .then(function(response) {
-        
-              // Récupère la donnée d'une API
-              const data = response.data;
 
-              // On récupère l'information principal
-              const main = data.weather[0].main;
-              const description = data.weather[0].description;
-              const temp = data.main.temp;
-              const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+// Création de l'objet apiWeather
+const apiWeather = new API_WEATHER();
 
-              // Modifier le DOM
-              document.getElementById('today-forecast-main').innerHTML = main;
-              document.getElementById('today-forecast-more-info').innerHTML = description;
-              document.getElementById('icon-weather-container').innerHTML = icon;
-              document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-      
-        })
+var city = document.getElementById("city-input").value;
     
-        .catch(function(error) {
+// Si aucune ville n'est définie
+if(city == "")
+{
+    city="Paris";
+}
+    
+// Appel de la fonction fetchTodayForecast
+apiWeather
+
+    .fetch3DaysForecast(city)
+    .then(function(response) {
+    
+        // Affiche ce que récupère le programme
+        console.log(response.data);
+
+        // Récupère la donnée d'une API
+        const data = response.data;
+
+        for(i = 0; i < 4; i++) {
+            
+            htmlElement = "today";
+            
+            if(i == 1) {
+                htmlElement = "tomorrow";
+            }
+            
+            else if(i == 2) {
+                htmlElement = "a-tomorrow";
+            }
+            
+            else if(i == 3) {
+                htmlElement = "a-a-tomorrow";
+            }
         
-            // Affiche une erreur
-            console.error(error);
-        
-        });
+            // On récupère l'information principal
+            const main = data.list[i].weather[0].main;
+            const description = data.list[i].weather[0].description;
+            const temp = data.list[i].temp.day;
+            const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
+
+            // Modifier le DOM
+            document.getElementById(htmlElement+'-forecast-main').innerHTML = main;
+            document.getElementById(htmlElement+'-forecast-more-info').innerHTML = description;
+            document.getElementById(htmlElement+'-icon-weather-container').innerHTML = icon;
+            document.getElementById(htmlElement+'-forecast-temp').innerHTML = `${temp}°C`;
+        }
+
+    })
+
+    .catch(function(error) {
+
+        // Affiche une erreur
+        console.error(error);
+
+    });
 }
